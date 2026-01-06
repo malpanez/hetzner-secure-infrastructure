77313ee6b5498156f4a35f120cead55a657fceda
Miguel Alpañez <alpanez.alcalde@gmail.com>
2026-01-06 13:12:35 +0000

    Add missing provider version constraints for local and null
    tflint was warning about missing version constraints for local and null providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,14 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }
 
