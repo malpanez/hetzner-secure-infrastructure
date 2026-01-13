5f55f534a46905079a54d13d4167cc50d119afea
Miguel Alpañez <alpanez.alcalde@gmail.com>
2026-01-13 06:02:42 +0000

    fix: update Cloudflare token permissions based on working homelabforge.dev config
    Changed terraform/main.tf provider version from ~> 5.0 to ~> 4.0 to match
    all other configurations. This resolves the conflicting version constraints
    error during terraform init.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 5.0"
+      version = "~> 4.0"
     }
     local = {
       source  = "hashicorp/local"
